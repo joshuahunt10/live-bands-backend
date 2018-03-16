59763029d38c6595aae3ea8be943251a0242e7e3
joshuahunt10 <joshua.hunt88@gmail.com>
2018-03-16 16:27:14 -0500

    basic query done in backend

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,29 @@
 const express = require('express')
 const router = express.Router()
+const API_KEY = require('../config')
+const fetch = require("node-fetch");
+
+const url = `https://app.ticketmaster.com/discovery/v2/events.json?classificationName=music&dmaId=324&apikey=${API_KEY}`
 
 router.get('/', function(req, res){
   res.json({
-    response: "This is working"
+    endpoints: {
+      fetchTicketmaster: "/fetchTicketmaster fetches the ticketmaster data"
+    }
   })
+})
+
+router.get('/fetchTicketmaster', function(req, res){
+  fetch(url)
+    .then(response => {
+      response.json().then(json => {
+        let j = json._embedded.events
+        res.json(j)
+      })
+    })
+    .catch(error => {
+      console.log(error);
+    });
 })
 
 module.exports = router;
